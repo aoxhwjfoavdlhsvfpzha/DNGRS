debfbb1e8bd270a7bbd165259b6c826944fbb01f
aoxhwjfoavdlhsvfpzha <58840974+aoxhwjfoavdlhsvfpzha@users.noreply.github.com>
2024-02-05 20:47:21 -0800

    Add files via upload

--- weird.py
+++ weird.py
@@ -29,26 +29,43 @@ for z in range(4):
         val += int(parities[z][y])
     pvals.append(val)
 
-print("Parity #1: ", end="")
+pbin = ""
+
+print("Parity bit #1: ", end="")
 if int(pvals[0]) % 2 == 0:
     print("1")
+    pbin += "1"
 else:
     print(0)
+    pbin += "0"
 
-print("Parity #2: ", end="")
+print("Parity bit #2: ", end="")
 if int(pvals[1]) % 2 == 0:
     print("0")
+    pbin += "0"
 else:
     print("1")
+    pbin += "1"
 
-print("Parity #3: ", end="")
+print("Parity bit #3: ", end="")
 if int(pvals[2]) % 2 == 0:
     print("0")
+    pbin += "0"
 else:
     print("1")
+    pbin += "1"
 
-print("Parity #4: ", end="")
+print("Parity bit #4: ", end="")
 if int(pvals[3]) % 2 == 0:
     print("0")
+    pbin += "0"
 else:
     print("1")
+    pbin += "1"
+
+number = pbin
+decinum = int(str(number), 2)
+hexinum = hex(decinum)[2:]
+
+print()
+print("Parity Hex: " + hexinum.upper())
